f7afe0bc5df4635aac26baca3124a63280d8daea
Vitchet Uk <vitchet.uk@hotmail.com>
2022-10-12 21:16:36 +0200

    add index.js

--- js/index.js
+++ js/index.js
@@ -27,4 +27,35 @@ if (hacker1.length > hacker2.length) {
 
 // Iteration 3: Loops
 
-console.log(hacker1.toUpperCase().split("").join(" "));
+let hacker1UpSpace = "";
+
+for (let i = 0; i < hacker1.length; i++) {
+  if ([i] === hacker1.length - 1) {
+    hacker1UpSpace += hacker1[i].toUpperCase();
+  } else {
+    hacker1UpSpace += hacker1[i].toUpperCase() + " ";
+  }
+}
+console.log(hacker1UpSpace);
+
+let hacker2Reverse = "";
+
+for (let i = hacker2.length - 1; i >= 0; i--) {
+  hacker2Reverse += hacker2[i];
+}
+
+console.log(hacker2Reverse);
+
+if (hacker1 === hacker2) {
+  console.log("What?! You both have the same name?");
+} else {
+  for (let i = 0; i < hacker1.length; i++) {
+    if (hacker1[i] < hacker2[i]) {
+      console.log("The driver's name goes first.");
+      break;
+    } else {
+      console.log("Yo, the navigator goes first definitely.");
+      break;
+    }
+  }
+}
